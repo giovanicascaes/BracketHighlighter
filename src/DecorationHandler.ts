359838d3441a84df7e4084850420482ba2e00e6d
Giovani Cascaes <giovani.cascaes@avenuecode.com>
2022-08-19 16:56:34 -0300

    Color bounce symbol

--- src/DecorationHandler.ts
+++ src/DecorationHandler.ts
@@ -29,13 +29,13 @@ export default class DecorationHandler {
             letterSpacing: decorationOptions.letterSpacing,
             outline: decorationOptions.outline,
             border: decorationOptions.border,
-            textDecoration: decorationOptions.textDecoration,
+            textDecoration: addAnimation ? 'none; animation-name: var(--bounce-bracket-symbol-animation-name); animation-duration: var(--bounce-bracket-symbol-animation-duration); animation-delay: var(--bounce-bracket-symbol-animation-delay)' : decorationOptions.textDecoration,
             backgroundColor: decorationOptions.backgroundColor,
             overviewRulerLane: vscode.OverviewRulerLane.Left,
             overviewRulerColor: decorationOptions.backgroundColor,
             after: addAnimation ? {
                 contentText: '',
-                textDecoration: 'none; position: absolute; z-index: -1px; top: 0; margin-left: -8px; z-index: -1; padding: 0 2px; width: 4px; height: calc(100% - 1px); border-radius: 2px; animation-name: var(--bounce-bracket-animation-name); animation-duration: var(--bounce-bracket-animation-duration); animation-delay: var(--bounce-bracket-animation-delay)',
+                textDecoration: 'none; position: absolute; z-index: -1px; top: 0; margin-left: -8px; z-index: -1; padding: 0 3px 1px 1px; width: 4px; height: calc(100% - 1px); border-radius: 2px; animation-name: var(--bounce-bracket-animation-name); animation-duration: var(--bounce-bracket-animation-duration); animation-delay: var(--bounce-bracket-animation-delay)',
             } : undefined,
         });
         return decorationType;
